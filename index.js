8890120318cf9af6fc526f5565f0d8e2d605fb0a
Dhanush16dd <dhanush16fk@gmail.com>
2023-03-12 22:28:09 +0530

    fully complet

--- index.js
+++ index.js
@@ -78,61 +78,6 @@ items[i].onclick = function(){
 }
 
 
-function output(input) {
-  let product;
-
-  // Regex remove non word/space chars
-  // Trim trailing whitespce
-  // Remove digits - not sure if this is best
-  // But solves problem of entering something like 'hi1'
-
-  let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
-  text = text
-    .replace(/ a /g, " ")   // 'tell me a story' -> 'tell me story'
-    .replace(/i feel /g, "")
-    .replace(/whats/g, "what is")
-    .replace(/please /g, "")
-    .replace(/ please/g, "")
-    .replace(/r u/g, "are you");
-
-  if (compare(prompts, replies, text)) { 
-    // Search for exact match in `prompts`
-    product = compare(prompts, replies, text);
-  } else if (text.match(/thank/gi)) {
-    product = "You're welcome!"
-  } else if (text.match(/(corona|covid|virus)/gi)) {
-    // If no match, check if message contains `coronavirus`
-    product = coronavirus[Math.floor(Math.random() * coronavirus.length)];
-  } else {
-    // If all else fails: random alternative
-    product = alternative[Math.floor(Math.random() * alternative.length)];
-  }
-
-  // Update DOM
-  addChat(input, product);
-}
-
-function compare(promptsArray, repliesArray, string) {
-  let reply;
-  let replyFound = false;
-  for (let x = 0; x < promptsArray.length; x++) {
-    for (let y = 0; y < promptsArray[x].length; y++) {
-      if (promptsArray[x][y] === string) {
-        let replies = repliesArray[x];
-        reply = replies[Math.floor(Math.random() * replies.length)];
-        replyFound = true;
-        // Stop inner loop when input value matches prompts
-        break;
-      }
-    }
-    if (replyFound) {
-      // Stop outer loop when reply is found instead of interating through the entire array
-      break;
-    }
-  }
-  return reply;
-}
-
 function addChat(input, product) {
   const messagesContainer = document.getElementById("messages");
 
@@ -235,18 +180,18 @@ items[i].onclick = function(){
 
 
 function showInfo(data,input){
-
+var product,key;
 for(let i= 0 ;i < data.intents.length; i++){
 
-  let product = compareRandom(data.intents[i].tag,data.intents[i].patterns,data.intents[i].responses,input);
+  product = compareRandom(data.intents[i].tag,data.intents[i].patterns,data.intents[i].responses,input);
+
   if (product) {
-  console.log(product)
-  
-  console.log(data.intents[i].tag)
+    console.log(product)
+    key = "seccuss"
   }
 
 }
-
+console.log(key)
 }
 
 function compareRandom(tag,promptsArray, repliesArray, string) {
@@ -280,7 +225,7 @@ if (tag == "single") {
         break;
       }
     }
-    text = "";
+    return reply
   }else if(tag == "all"){
     let reply;
     let replyFound = false;
@@ -298,20 +243,8 @@ if (tag == "single") {
         // Stop outer loop when reply is found instead of interating through the entire array
         break;
       }
-    }return null
-   }//else{
-  //   const alternative = [
-  //     "Same",
-  //     "Go on...",
-  //     "Bro...",
-  //     "Try again",
-  //     "I'm listening...",
-  //     "I don't understand :/"
-  //   ]
-  //   let replies = alternative[Math.floor(Math.random() * alternative.length)];
-  //   console.log(replies)
-  //   addChat(text, replies)
-  // }
+    }return reply
+   }
     
   }
 
